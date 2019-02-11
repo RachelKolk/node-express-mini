b1ada8c5bed17100cd172e17d9f3e7b0ff12dbaa
RachelKolk <rach.m.obrien@gmail.com>
2019-02-11 16:32:05 -0500

    working on post request

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 // implement your API here
 const express = require('express');
 
-const db = require('./data/seeds/db.js');
+const db = require('./data/db.js');
 
 const server = express();
 
 server.use(express.json());
+
+
+//endpoints
+
+server.post('/api/users', (req, res) => {
+    const user = req.body
+    db
+    .insert(user)
+    .then(user => {
+        res.status(201).json({success: true, user});
+    }) 
+    .catch = () => {
+        res.status(500).json({success: false, user});
+    };
+});
+
+
+server.get('/api/users', (req, res) => {
+    db
+    .find()
+    .then(users => {
+        res.status(200).json({success: true, users});
+    })
+    .catch = () => {
+        res.status(500).json({ error: "The users information could not be retrieved." });
+    };
+});
+
+server.listen(4000, () => {
+    console.log('\n*** Running on port 4000 ***\n');
+}); 
