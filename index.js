2b2d9c1b781eaf3ec10a1a13d0b787a30493326c
RachelKolk <rach.m.obrien@gmail.com>
2019-02-11 17:11:31 -0500

    get by id request working with all requirements

--- index.js
+++ index.js
@@ -11,15 +11,20 @@ server.use(express.json());
 //endpoints
 
 server.post('/api/users', (req, res) => {
-    const user = req.body
-    db
-    .insert(user)
-    .then(user => {
-        res.status(201).json({success: true, user});
-    }) 
-    .catch = () => {
-        res.status(500).json({success: false, user});
-    };
+    const user = req.body;
+    console.log(user);
+    if (user.name == null || user.bio == null) {
+        return res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+        db
+        .insert(user)
+        .then(user => {
+            res.status(201).json({success: true, user});
+        }) 
+        .catch = () => {
+            res.status(500).json({error: "There was an error while saving the user to the database" });
+        };
+    }   
 });
 
 
@@ -34,6 +39,25 @@ server.get('/api/users', (req, res) => {
     };
 });
 
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db
+      .findById(id)
+      .then(user => {
+          if (user) {
+            res.status(201).json({success: true, user});
+          } else {
+              res.status(404).json({ message: "The user with the specified ID does not exist." });
+          }   
+      })
+      .catch= () => {
+        res.status(500).json({ error: "The user information could not be retrieved." });
+      };
+});
+
 server.listen(4000, () => {
     console.log('\n*** Running on port 4000 ***\n');
-}); 
+});
+
+
