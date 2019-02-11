023cd121f2022cc15f036513ad2d744add106ec2
RachelKolk <rach.m.obrien@gmail.com>
2019-02-11 17:29:18 -0500

    delete request working

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ server.use(express.json());
 
 //endpoints
 
+
+//  POST - creates a user using the information sent inside the request body
 server.post('/api/users', (req, res) => {
     const user = req.body;
     console.log(user);
@@ -28,6 +30,7 @@ server.post('/api/users', (req, res) => {
 });
 
 
+//  GET - returns an array of all of the users contained in the database
 server.get('/api/users', (req, res) => {
     db
     .find()
@@ -39,6 +42,8 @@ server.get('/api/users', (req, res) => {
     };
 });
 
+
+//  GET by ID - returns the user object with the specified id
 server.get('/api/users/:id', (req, res) => {
     const id = req.params.id;
 
@@ -51,8 +56,27 @@ server.get('/api/users/:id', (req, res) => {
               res.status(404).json({ message: "The user with the specified ID does not exist." });
           }   
       })
-      .catch= () => {
+      .catch = () => {
         res.status(500).json({ error: "The user information could not be retrieved." });
+      };
+});
+
+
+//  DELETE - removes the user with the specified id and also returns that user
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db
+      .remove(id)
+      .then(user => {
+          if (user) {
+              res.status(201).json({success: true, user});
+          } else {
+              res.status(404).json({ message: "The user with the specified ID does not exist."});
+          }
+      })
+      .catch = () => {
+          res.status(500).json({ error: "The user could not be removed" });
       };
 });
 
